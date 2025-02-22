297152ed58e762afdc5921a50ef3fa672bbe9493
Ömer ÖZYILMAZ <omerozylmaz2@gmail.com>
2025-02-22 13:43:23 +0300

    Update 0.2.2 Form Element Clean Code and Costom Hooks

--- job-board/src/components/JobColumn.jsx
+++ job-board/src/components/JobColumn.jsx
@@ -1,53 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import JobCard from "./JobCard";
-import { useDispatch, useSelector } from "react-redux";
-import { addJob, moveJob } from "../store/jobSlice";
-import { updateFormData, resetFormData } from "../store/formSlice";
+import { useSelector } from "react-redux";
+import useFormHandler from "../hooks/useFormHandler";
+import useDragAndDrop from "../hooks/useDragAndDrop";
 
 const JobColumn = ({ title, columnKey }) => {
-  const dispatch = useDispatch();
   const jobs = useSelector((state) => state.jobs[columnKey]);
-  const formData = useSelector((state) => state.form.formData);
+  const {
+    isFormVisible,
+    toggleFormVisibility,
+    handleInputChange,
+    handleFormSubmit,
+    formData,
+  } = useFormHandler(columnKey);
 
-  // Local state for form visibility
-  const [isFormVisible, setFormVisible] = useState(false);
-
-  const handleAddJob = () => {
-    setFormVisible(!isFormVisible);
-  };
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    dispatch(updateFormData({ [name]: value }));
-  };
-
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    const newJob = {
-      ...formData,
-      addedTime: "Just now",
-    };
-    dispatch(addJob({ column: columnKey, job: newJob }));
-    setFormVisible(false);
-    dispatch(resetFormData());
-  };
-
-  const handleDragStart = (e, index) => {
-    e.dataTransfer.setData("jobIndex", index);
-    e.dataTransfer.setData("fromColumn", columnKey);
-  };
-
-  const handleDrop = (e) => {
-    const jobIndex = e.dataTransfer.getData("jobIndex");
-    const fromColumn = e.dataTransfer.getData("fromColumn");
-    if (fromColumn !== columnKey) {
-      dispatch(moveJob({ fromColumn, toColumn: columnKey, jobIndex }));
-    }
-  };
-
-  const handleDragOver = (e) => {
-    e.preventDefault();
-  };
+  const { handleDragStart, handleDrop, handleDragOver } =
+    useDragAndDrop(columnKey);
 
   return (
     <div
@@ -60,7 +28,7 @@ const JobColumn = ({ title, columnKey }) => {
           {title} ({jobs.length})
         </h2>
         <button
-          onClick={handleAddJob}
+          onClick={toggleFormVisibility}
           className="bg-blue-500 text-white px-2 py-1 rounded"
         >
           +
